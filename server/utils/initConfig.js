334ecb84b6915afd2f50bf5b8e7743d07f19269e
ShoneSingLone <ShoneSingLone@gmail.com>
2023-02-28 19:55:23 +0800

    configs sample

--- server/utils/initConfig.js
+++ server/utils/initConfig.js
@@ -1,5 +1,30 @@
 global.WEBCONFIG = require('../../../yapiConfigs.js');
 
+
+/* 
+
+module.exports = {
+    port: "0000",
+    adminAccount: "xxx@xxx.xxx",
+    db: {
+        servername: "xx.xx.xx.xx",
+        DATABASE: "xx",
+        port: "xxxx",
+    },
+    mail: {
+        enable: true,
+        host: "smtp.qq.com",
+        port: xxx,
+        from: "xxxxxx",
+        auth: {
+            user: "xxxxxxx",
+            pass: "xxxxxx",
+        },
+    },
+};
+
+*/
+
 exports.initDbAndCommon = async function () {
   require('../yapi.js');
   require('../utils/commons').setYapiCommons();
