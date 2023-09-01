6319e47ebe29d2d0926f4564a29cbd7aa4729839
ShoneSingLone <ShoneSingLone@gmail.com>
2023-09-02 00:35:19 +0800

    update

--- server/middleware/historyMode.js
+++ server/middleware/historyMode.js
@@ -5,19 +5,26 @@ const mime = require("mime-types");
 const middlewareNotFound = () => async (ctx, next) => {
 	/* history 模式，除了api，都返回index.html */
 	if (ctx.status === 404) {
-		xU.applog.info("middlewareNotFound return index", ctx.path);
-		let indexPath = path.join(xU.var.APP_ROOT_DIR, "static", ctx.path.substring(1));
-		if (xU.fileExist(indexPath)) {
-			ctx.status = 200;
-			ctx.set("Content-Type", mime.lookup(indexPath));
-			ctx.body = xU.fs.createReadStream(indexPath);
-		} else {
+
+		(() => {
+			xU.applog.info("middlewareNotFound return index", ctx.path);
+			let indexPath;
+
+			indexPath = path.join(xU.var.APP_ROOT_DIR, "static", ctx.path.substring(1));
+			if (xU.fileExist(indexPath)) {
+				ctx.status = 200;
+				ctx.set("Content-Type", mime.lookup(indexPath));
+				ctx.body = xU.fs.createReadStream(indexPath);
+				return;
+			}
+
 			/* 返回首页 */
 			indexPath = path.join(xU.var.APP_ROOT_DIR, "static", "index.html");
 			ctx.status = 200;
 			ctx.set("Content-Type", mime.lookup(indexPath));
 			ctx.body = xU.fs.createReadStream(indexPath);
-		}
+			return;
+		})();
 	}
 	next();
 };
