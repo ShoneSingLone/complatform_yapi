b0fbb9d05a0d82ddb85e1bc8adb4aedc04e91404
ShoneSingLone <ShoneSingLone@gmail.com>
2024-06-22 11:56:42 +0800

    add proxy

--- server/assets.ts
+++ server/assets.ts
@@ -1,7 +1,11 @@
 const { middlewareGzipWhenProd } = require("server/middleware/prdGzip");
 const { middlewareNormalAssets } = require("server/middleware/normalAssets");
+const proxy = require("koa2-proxy-middleware");
 
 module.exports = async function (app) {
+	if (global.yapi_configs?.proxyOptions) {
+		app.use(proxy(global.yapi_configs.proxyOptions));
+	}
 	app.use(middlewareGzipWhenProd());
 	/* static */
 	app.use(middlewareNormalAssets());
