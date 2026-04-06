91be6b1daaae2d4e43770acba233081e469eed11
ShoneSingLone <ShoneSingLone@gmail.com>
2026-04-06 20:59:37 +0800

    ``` chore(DocFlow): 更新子模块状态
    标记DocFlow子模块为dirty状态，表明存在未提交的更改
    ```

---
+++ static/business_yapi/views/V1/ViewXspace.vue
@@ -0,0 +1,17 @@
+<template>
+	<DesktopWorkspace />
+</template>
+
+<script lang="ts">
+export default async function ({ PRIVATE_GLOBAL }) {
+	const [DesktopWorkspace] = await _.$importVue([
+		"@/components/DesktopWorkspace/DesktopWorkspace.vue"
+	]);
+
+	return {
+		components: {
+			DesktopWorkspace
+		}
+	};
+}
+</script>
